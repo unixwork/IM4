0bac281b1a217a16fc195c328382d01ad61a136e
Olaf Wintermann <olaf.wintermann@gmail.com>
2023-08-11 09:50:12 +0200

    Initial Commit

---
+++ IM4/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  IM4
+//
+//  Created by Olaf Wintermann on 11.08.23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ IM4/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  IM4
+//
+//  Created by Olaf Wintermann on 11.08.23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
