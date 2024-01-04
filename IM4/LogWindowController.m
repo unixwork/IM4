c344a0de0eefa85e9440a6771164e4499da419b4
Olaf Wintermann <olaf.wintermann@gmail.com>
2024-01-04 21:47:05 +0100

    buffer log messages, before the log window is loaded

--- IM4/LogWindowController.m
+++ IM4/LogWindowController.m
@@ -9,25 +9,37 @@
 
 @interface LogWindowController ()
 
+@property NSMutableString *buffer;
+
 @end
 
 @implementation LogWindowController
 
 - (id)initLogWindow {
     self = [self initWithWindowNibName:@"LogWindowController"];
+    _buffer = [[NSMutableString alloc] init];
+    
     return self;
 }
 
 - (void)windowDidLoad {
     [super windowDidLoad];
+    if(_buffer != nil) {
+        size_t bufLen = _buffer.length;
+        const char *bufStr = _buffer.UTF8String;
+        _buffer = nil;
+        [self addToLog:bufStr length:bufLen];
+    }
 }
 
 - (void)addToLog:(const char *)str length:(size_t)length {
-    NSData *data = [NSData dataWithBytes:str length:length];
-    NSDictionary *options = @{NSCharacterEncodingDocumentAttribute: @(NSUTF8StringEncoding)};
-    NSAttributedString *attributedText = [[NSAttributedString alloc] initWithHTML:data
-                                                                          options:options
-                                                               documentAttributes:nil];
+    NSString *logStr = [[NSString alloc]initWithBytes:str length:length encoding:NSUTF8StringEncoding ];
+    if(!self.windowLoaded) {
+        [_buffer appendString:logStr];
+        return;
+    }
+    
+    NSAttributedString *attributedText = [[NSAttributedString alloc] initWithString:logStr];
     
     [_log.textStorage appendAttributedString:attributedText];
     [_log scrollToEndOfDocument:nil];
