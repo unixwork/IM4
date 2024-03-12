ead644773dfac0dec262528922259241063850c6
Olaf Wintermann <olaf.wintermann@gmail.com>
2024-03-12 12:12:05 +0100

    update libgpg-error dependency to version 1.48

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -11,20 +11,20 @@ DEPDIR=dep
 # dependency downloads
 DL_OPENSSL=https://www.openssl.org/source/openssl-3.2.1.tar.gz
 DL_LIBSTROPHE=https://github.com/strophe/libstrophe/releases/download/0.12.3/libstrophe-0.12.3.tar.bz2
-DL_LIBGPGERR=https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.47.tar.bz2
+DL_LIBGPGERR=https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.48.tar.bz2
 DL_LIBGCRYPT=https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.10.3.tar.bz2
 DL_LIBOTR=https://otr.cypherpunks.ca/libotr-4.1.1.tar.gz
 
 SHA256_OPENSSL="83c7329fe52c850677d75e5d0b0ca245309b97e8ecbcfdc1dfdc4ab9fac35b39  openssl-3.2.1.tar.gz"
 SHA256_LIBSTROPHE="1a978b244ca6bacfec60f2a916ac3ce7c564fa1723a6d2a89e458ad457c6574f  libstrophe-0.12.3.tar.bz2"
-SHA256_LIBGPGERR="9e3c670966b96ecc746c28c2c419541e3bcb787d1a73930f5e5f5e1bcbbb9bdb  libgpg-error-1.47.tar.bz2"
+SHA256_LIBGPGERR="89ce1ae893e122924b858de84dc4f67aae29ffa610ebf668d5aa539045663d6f  libgpg-error-1.48.tar.bz2"
 SHA256_LIBGCRYPT="8b0870897ac5ac67ded568dcfadf45969cfa8a6beb0fd60af2a9eadc2a3272aa  libgcrypt-1.10.3.tar.bz2"
 SHA256_LIBOTR="8b3b182424251067a952fb4e6c7b95a21e644fbb27fbd5f8af2b2ed87ca419f5  libotr-4.1.1.tar.gz"
 
 
 DIR_OPENSSL=openssl-3.2.1
 DIR_LIBSTROPHE=libstrophe-0.12.3
-DIR_LIBGPGERR=libgpg-error-1.47
+DIR_LIBGPGERR=libgpg-error-1.48
 DIR_LIBGCRYPT=libgcrypt-1.10.3
 DIR_LIBOTR=libotr-4.1.1
 
@@ -57,7 +57,7 @@ done
 # check download hashes
 DL_OPENSSL_SHA256=$(shasum -a 256 openssl-3.2.1.tar.gz)
 DL_LIBSTROPHE_SHA256=$(shasum -a 256 libstrophe-0.12.3.tar.bz2)
-DL_LIBGPGERR_SHA256=$(shasum -a 256 libgpg-error-1.47.tar.bz2)
+DL_LIBGPGERR_SHA256=$(shasum -a 256 libgpg-error-1.48.tar.bz2)
 DL_LIBGCRYPT_SHA256=$(shasum -a 256 libgcrypt-1.10.3.tar.bz2)
 DL_LIBOTR_SHA256=$(shasum -a 256 libotr-4.1.1.tar.gz)
 
